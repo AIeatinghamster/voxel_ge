0bb111e1e0b9fab44a5a60e1d642886c4ee92731
AIeatinghamster <vito.p.jokic@gmail.com>
2026-07-31 20:46:10 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,58 @@
 #include <string>
 #include <vector>
 
+#include <SDL3/SDL.h>
+//#include <SDL3/SDL_main.h>
+#include <bgfx/bgfx.h>
+#include <bgfx/platform.h>
+#include <bx/math.h>
 #include <cstdint>
 #include <imgui.h>
 #include <unordered_map>
+#include <fstream>
+
+std::string vs="shaders/vs_voxel.bin";
+std::string fs="shaders/fs_voxel.bin";
+/*bgfx::ShaderHandle loadShader(const char* path){
+    std::ifstream file(path,std::ios::binary|std::ios::ate);
+    if (!file){
+        std::cout<<"failed to load file " << path;
+        return BGFX_INVALID_HANDLE;
+    };
+    std::cout << "1\n";
+    std::streamsize size=file.tellg();
+    std::cout << "2\n";
+    file.seekg(0);
+    std::cout << "3\n";
+    const bgfx::Memory* mem=bgfx::alloc((uint32_t)size);
+    std::cout << "4\n";
+    file.read((char*)mem->data,size);
+    std::cout << "5\n";
+    return bgfx::createShader(mem);
+}*/
+bgfx::ShaderHandle loadShader(const char* path){
+    FILE* file=fopen(path,"rb");
+    if (!file){
+        std::cout<<"failed to load file " << path;
+        return BGFX_INVALID_HANDLE;
+    };
+    std::cout << "1\n";
+    fseek(file,0,SEEK_END);
+    std::cout << "2\n";
+    uint32_t size = static_cast<uint32_t>(ftell(file));
+    std::cout << "3 " << size << "\n";
+    fseek(file,0,SEEK_SET);
+    std::cout << "4\n";
+    const bgfx::Memory* mem=bgfx::alloc(size);
+    std::cout << "5\n";
+    fread(mem->data,1,size,file);
+    std::cout << "6\n";
+    fclose(file);
+    std::cout << "7\n";
+    mem->data[mem->size-1]='\0';
+    std::cout << "8\n";
+    return bgfx::createShader(mem);
+}
 int floorDiv(int a, int b)
 {
     if(a >= 0)
@@ -185,14 +234,17 @@ class Structure{
 bool isAir(int globalX, int globalY, int globalZ);
 
 class Chunk{
-    ChunkPos position;
+    ChunkPos position{0,0};
     public:
         std::vector<Block> blocks;
         Mesh mesh;
         bool dirty = false;
-
-        Chunk(ChunkPos pos)
-            : position(pos){
+        bgfx::VertexBufferHandle vbh=BGFX_INVALID_HANDLE;
+        bgfx::IndexBufferHandle ibh=BGFX_INVALID_HANDLE;
+        Chunk(){
+            blocks.resize(chunk_size * chunk_height * chunk_size);
+        }
+        Chunk(ChunkPos pos) : position(pos){
             blocks.resize(chunk_size * chunk_height * chunk_size);
         }
 
@@ -200,7 +252,12 @@ class Chunk{
         {
             return blocks[index(x,y,z)];
         }
+        void init(){
+            layout.begin().add(bgfx::Attrib::Position,3, bgfx::AttribType::Float).add(bgfx::Attrib::TexCoord0, 2, bgfx::AttribType::Float).end();
+        }
+        bgfx::VertexLayout layout;
     void make_mesh(){
+        std::cout<< "meshing\n";
         mesh.vertices.clear();
         mesh.indices.clear();
         for (int x=0;x<chunk_size; x++){
@@ -234,18 +291,32 @@ class Chunk{
                         mesh.addFace(5,x,y,z);
                     }
                     //y+
-                    if ((isAir(global_blockX,y+1,global_blockZ))){
+                    if ((isAir(global_blockX,y-1,global_blockZ))){
                         //faces_to_show_in_this_block.push_back({x,y+1,z})
                         mesh.addFace(0,x,y,z);
                     }
                     //y-
-                    if (isAir(global_blockX,y-1,global_blockZ)){
+                    if (isAir(global_blockX,y+1,global_blockZ)){
                         //faces_to_show_in_this_block.push_back({x,y-1,z})
                         mesh.addFace(1,x,y,z);
                     }
                 }
             }
         }
+        std::cout<< "meshed\n";
+
+    }
+    void make_layout(){
+        if (bgfx::isValid(vbh)){
+            bgfx::destroy(vbh);
+        }
+        if (bgfx::isValid(ibh)){
+            bgfx::destroy(ibh);
+        }
+        vbh = bgfx::createVertexBuffer(bgfx::copy(mesh.vertices.data(), mesh.vertices.size()*sizeof(Vertex)),layout);
+        ibh = bgfx::createIndexBuffer(bgfx::copy(mesh.indices.data(),mesh.indices.size()*sizeof(uint32_t)), BGFX_BUFFER_INDEX32);
+        std::cout << "Vertices: " << mesh.vertices.size() << '\n';
+        std::cout << "Indices: " << mesh.indices.size() << '\n';
     }
 };
 std::unordered_map<ChunkPos, Chunk> loaded_chunks;
@@ -268,7 +339,7 @@ bool isAir(int globalX, int globalY, int globalZ){
 
     return !chunk.getBlock(localX, globalY, localZ).isSolid();
 }
-
+Mesh world_mesh;
 void generateChunks(){
     //tmp ignor rand_noise
     Chunk chunk({0,0});
@@ -288,6 +359,76 @@ void generateChunks(){
 
     loaded_chunks[{0,0}].make_mesh();
 }
+bool running=true;
+int width=1080;
+int height=900;
 int main(){
+    generateChunks();
+    SDL_Init(SDL_INIT_VIDEO);
+    if (!SDL_Init(SDL_INIT_VIDEO)){
+        std::cout << "failed sdl init\n";
+        return -1;
+    }
+    SDL_Window* window=SDL_CreateWindow("VOXEL_GE",width,height, SDL_WINDOW_RESIZABLE);
+    if (!window){
+        std::cout << "failed wimdow_creation\n";
+        return -1;
+    }
+    bgfx::PlatformData pd{};
+    SDL_PropertiesID props=SDL_GetWindowProperties(window);
+    //#ifdef _WIN32
+    pd.nwh=(void*)SDL_GetPointerProperty(props,SDL_PROP_WINDOW_WIN32_HWND_POINTER,nullptr);
+    bgfx::setPlatformData(pd);
+    //#endif
+    std::cout << pd.nwh << "\n";
+    bgfx::Init init;
+    init.type=bgfx::RendererType::Count;
+    init.resolution.height=height;
+    init.resolution.width=width;
+    init.resolution.reset=BGFX_RESET_VSYNC;
+    init.platformData.nwh=pd.nwh;
+    bgfx::init(init);
+    bgfx::ShaderHandle vsh=loadShader("C:\\Users\\vitop\\Desktop\\GAME_ENGINE\\src\\vs_voxel.bin");
+    bgfx::ShaderHandle fsh=loadShader("C:\\Users\\vitop\\Desktop\\GAME_ENGINE\\src\\fs_voxel.bin");
+    bgfx::ProgramHandle program=bgfx::createProgram(vsh,fsh,true);
+    float view[16];
+    bx::mtxLookAt(
+        view,
+        bx::Vec3{20.0f, 20.0f, 20.0f},   // camera position
+        bx::Vec3{0.0f, 0.0f, 0.0f}       // look at
+    );
+
+    float proj[16];
+    bx::mtxProj(
+        proj,
+        60.0f,
+        float(width) / float(height),
+        0.1f,
+        1000.0f,
+        bgfx::getCaps()->homogeneousDepth
+    );
+
+    bgfx::setViewTransform(0, view, proj);
+    while (running){
+        for (auto& [ChunkPos,chunk]:loaded_chunks){
+            chunk.init(); 
+            chunk.make_layout();
+            if (!bgfx::isValid(chunk.vbh)){
+                std::cout << "no_chunks\n";
+                continue;
+            }
+            
+            bgfx::setViewClear(0,BGFX_CLEAR_COLOR|BGFX_CLEAR_DEPTH, 0x303030ff);
+            bgfx::setViewRect(0,0,0,width,height);
+            //bgfx::setState(BGFX_STATE_WRITE_RGB|BGFX_STATE_WRITE_Z|BGFX_STATE_DEAPTH_TEST_LESS|BGFX_CULL_CCW);
+            bgfx::setVertexBuffer(0,chunk.vbh);
+            bgfx::setIndexBuffer(chunk.ibh);
+            bgfx::submit(0,program);
+            bgfx::touch(0);
+        }
+        bgfx::frame();
+        //input
+    };
+    SDL_Quit();
     return 0;
-}
+};
